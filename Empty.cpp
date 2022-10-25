2526ce5e93e76c7660b3f6daa5c8ad1df701aa7f
Dimo Markov <dimomarkov@gmail.com>
2022-10-26 00:31:44 +0300

    Attempting to build the shared library

---
+++ Empty.cpp
@@ -0,0 +1,3 @@
+/// This file is intentionally left blank, in order to allow for              
+/// add_library(Langulus.Framework SHARED Empty.cpp)                          
+/// Read this for reasons: https://stackoverflow.com/questions/14347107       

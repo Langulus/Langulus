ebccafac8e4e41b3ed8c8bf08ed75c6fe25b26f3
Epixu <dimomarkov@gmail.com>
2022-11-03 18:26:51 +0200

    More tests passed

--- testmodule/Main.hpp
+++ testmodule/Main.hpp
@@ -9,3 +9,34 @@
 #include <Langulus.hpp>
 
 using namespace Langulus;
+using namespace Langulus::Flow;
+using namespace Langulus::Anyness;
+using namespace Langulus::Entity;
+
+class TestModule : public Module {
+   LANGULUS(ABSTRACT) false;
+
+   TestModule(Runtime* runtime, const Any&) noexcept
+      : Module {MetaOf<TestModule>(), runtime} {}
+
+   virtual void Update(Time) final {
+
+   }
+};
+
+struct SomeReflectedType1 {
+
+};
+
+struct SomeReflectedType2 {
+
+};
+
+struct SomeCategory {
+
+};
+
+LANGULUS_DEFINE_MODULE(TestModule, 1, "TestModule", "Just some module for testing", ""
+   , MetaOf<SomeCategory>()
+   , SomeReflectedType1, SomeReflectedType2
+)

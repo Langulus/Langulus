af50ccdc275daeba0fc4c5cedfdf57c3ea42afb6
Dimo Markov <dimomarkov@gmail.com>
2023-05-15 11:54:19 +0300

    Modernized CMake; Removed cmake submodule - uses FetchContent instead; Improved workflow; Warning fixes

--- framework/Empty.cpp
+++
@@ -1,3 +0,0 @@
-/// This file is intentionally left blank, in order to allow for              
-/// add_library(Langulus.Framework SHARED Empty.cpp)                          
-/// Read this for reasons: https://stackoverflow.com/questions/14347107       
